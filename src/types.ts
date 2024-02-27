0ff11e3a7c697ae6c2132262d78c099743e1c830
yaron96fromkoitechs <yaron96fromkoitechs@gmail.com>
2024-02-27 16:44:19 +0200

    fix: types for DI

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,23 @@
 export const TYPES = {
+  IUserRepository: Symbol.for('IUserRepository'),
+  IUserService: Symbol.for('IUserService'),
+  IMealRepository: Symbol.for('IMealRepository'),
+  IMealService: Symbol.for('IMealService'),
+
   ILoggerService: Symbol.for('ILoggerService'),
   IConfigService: Symbol.for('IConfigService'),
   IFileService: Symbol.for('IFileService'),
   IGptService: Symbol.for('IGptService'),
+  IAssistantService: Symbol.for('IAssistantService'),
+  PrismaService: Symbol.for('IPrismaService'),
+  TelegramService: Symbol.for('TelegramService'),
+
+  QueueService: Symbol.for('QueueService'),
 
   TelegramBot: Symbol.for('TelegramBot'),
   ChatSceneHandler: Symbol.for('ChatSceneHandler'),
   UserSettingsSceneHandler: Symbol.for('UserSettingsSceneHandler'),
+  MealSceneHandler: Symbol.for('MealSceneHandler'),
 
   WebServer: Symbol.for('WebServer'),
   IChatController: Symbol.for('IChatController'),
